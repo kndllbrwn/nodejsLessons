2e46b730099eef3af1e2ac4af4cb6ce3d1dd42bd
kndllbrwn <kndllbrwn1914@gmail.com>
2017-01-11 23:05:19 -0500

    installed lodash npm and utilized it in app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash')
 const notes = require('./notes.js')
 
-var sum = notes.notesAdd(2,2);
-console.log(sum);
+// console.log(_.isString(true));
+// console.log(_.isString('hi'));
+var filteredArray = _.uniq(["ken", 1, "ken", 2, 1, 3, 5])
+console.log(filteredArray);
 
 // var user = os.userInfo();
 
