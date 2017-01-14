fd4a84ffbd02be91068a895d01738c22bc302d8b
kndllbrwn <kndllbrwn1914@gmail.com>
2017-01-13 22:11:12 -0500

    added yargs npm as a way of parsing parameters passed to app.js and removed process

--- notes.js
+++ notes.js
@@ -1,7 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
 };
 
+var getAll = () => {
+    console.log("Getting all notes")
+};
+
+var getNote = (title) => {
+    console.log("Fetching note", title);
+};
+
+var removeNote = (title) => {
+    console.log("Removing note", title);
+};
+
+module.exports = {
+   addNote, // pre ES6 sytax = addNote : addNote
+   getAll, // pre ES6 sytax = getAll : getAll
+   getNote,
+   removeNote
+}; 
