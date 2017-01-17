b6e7e8761534262bc3bc7bdd6230d241ef11d3c2
kndllbrwn <kndllbrwn1914@gmail.com>
2017-01-16 19:12:27 -0500

    Walked through some debugging, added list notes functionality, and deleted some unnecessary lines

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -32,7 +30,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-    console.log("Getting all notes")
+    return fetchNotes();
 };
 
 var getNote = (title) => {
@@ -51,8 +49,9 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
-    console.log(note.title);
+    debugger;
     console.log('------');
+    console.log(note.title);
     console.log(note.body);
 };
 
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'Ken'
+};
+
+person.age = 29;
+
+debugger;
+
+person.name = 'Mike'
+
+console.log(person);
